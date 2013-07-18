2fe6cf5954c4670c37638a3cb0f97388e833ed30
Gian Carlo Val Ebao <gianebao@gmail.com>
2013-07-18 10:36:46 +0800

    added aws factory

---
+++ classes/AWS.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class AWS extends AWS_Core {}
---
+++ classes/AWS/Core.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+require 'vendor/autoload.php';
+
+class AWS_Core extends Aws\Common\Aws {
+    
+}
